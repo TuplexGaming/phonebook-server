2c935abe7e217da748bf027dc99a8d3165c576d8
TuplexGaming <tuplexgaming@gmail.com>
2022-07-07 08:50:14 -0600

    exercise 3.2

--- index.js
+++ index.js
@@ -36,6 +36,15 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+  const length = persons.length;
+  const date = new Date();
+  response.send(`
+    <p>Phonebook has info for ${length} people</p>
+    <p>${date}</p>
+  `);
+});
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
