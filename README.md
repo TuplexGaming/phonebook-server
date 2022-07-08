c56229a999eb6244c318654d2be5597a90b01c8c
TuplexGaming <tuplexgaming@gmail.com>
2022-07-08 09:03:57 -0600

    deploy to Heroku

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # phonebook-server
 
 Back end for Full Stack Open 2022 phonebook app, using RESTful API.
+https://guarded-ridge-18785.herokuapp.com/ | https://git.heroku.com/guarded-ridge-18785.git
--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
@@ -104,8 +106,12 @@ app.get('/info', (request, response) => {
   `);
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unkown endpoint' })
+}
+app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
