34f50f24d625426cbf99540667199c98a405c7a4
TuplexGaming <tuplexgaming@gmail.com>
2022-07-08 19:47:16 -0600

    exercises 3.13-3.14

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// npm run dev    start the server in dev mode with nodemon
-// npm start      start the server without nodemon
-
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
+
 const app = express();
 
 app.use(express.json());
@@ -13,51 +13,20 @@ app.use(cors());
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-];
-
-const generateId = () => {
-  const maxId = 100000;
-  return Math.floor(Math.random() * maxId);
-}
-
-
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook back end</h1>');
 });
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons);
+  });
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(p => p.id === id);
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -74,27 +43,20 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body;
 
-  if (!body.name) {
+  if (body.name === undefined) {
     return response.status(400).json({ error: 'name is missing' });
 
-  } else if (!body.number) {
+  } else if (body.number === undefined) {
     return response.status(400).json({ error: 'number is missing' });
 
   } else {
-    // Reject if the name matches any already in the array
-    const matchedPersons = persons.filter(p => p.name === body.name);
-    if (matchedPersons.length > 0) {
-      return response.status(400).json({ error: 'name must be unique' });
-    }
-
-    // Add new person if required data is available
-    const person = {
-      id: generateId(),
+    const person = new Person({
       name: body.name,
       number: body.number,
-    }
-    persons = persons.concat(person);
-    response.json(person);
+    });
+    person.save().then(savedPerson => {
+      response.json(savedPerson);
+    })
   }
 });
 
@@ -112,7 +74,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
