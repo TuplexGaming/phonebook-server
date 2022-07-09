cbcdfa395f67ba0117869ef198c75ec6ebc9fd32
TuplexGaming <tuplexgaming@gmail.com>
2022-07-09 08:11:53 -0600

    exercises 3.15-3.18

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 const Person = require('./models/person');
 
 const app = express();
 
-app.use(express.json());
+
+// Set up middleware
 app.use(express.static('build'));
-app.use(cors());
+app.use(express.json());
 
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
@@ -23,21 +23,20 @@ app.get('/api/persons', (request, response) => {
   });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person);
-  });
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person);
+    })
+    .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(p => p.id === id);
-  if (person) {
-    persons = persons.filter(p => p.id !== id);
-    response.status(204).end();
-  } else {
-    response.status(404).end();
-  }
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -60,20 +59,48 @@ app.post('/api/persons', (request, response) => {
   }
 });
 
-app.get('/info', (request, response) => {
-  const length = persons.length;
-  const date = new Date();
-  response.send(`
-    <p>Phonebook has info for ${length} people</p>
-    <p>${date}</p>
-  `);
+app.put('/api/persons/:id', (request, response) => {
+  const body = request.body;
+
+  const person = { name: body.name, number: body.number };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error));
 });
 
+app.get('/info', (request, response) => {
+  Person.countDocuments({}, (err, count) => {
+    const date = new Date();
+    response.send(`
+      <p>Phonebook has info for ${count} people</p>
+      <p>${date}</p>
+    `);
+  });
+});
+
+
+// Error handling
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unkown endpoint' })
 }
 app.use(unknownEndpoint);
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+}
+app.use(errorHandler);
+
+
+// Run the app...
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
