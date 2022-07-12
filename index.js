c101c94b7b05d65d71f8efdbac7f7c365970ad73
TuplexGaming <tuplexgaming@gmail.com>
2022-07-12 11:40:03 -0600

    exercises 3.19-3.21

--- index.js
+++ index.js
@@ -13,16 +13,19 @@ app.use(express.json());
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
+// Front page
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook back end</h1>');
 });
 
+// Get all persons
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons);
   });
 });
 
+// Get person by ID
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
@@ -31,6 +34,7 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
+// Delete person
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
@@ -39,38 +43,46 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+// Create new person
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
-
   if (body.name === undefined) {
     return response.status(400).json({ error: 'name is missing' });
-
   } else if (body.number === undefined) {
     return response.status(400).json({ error: 'number is missing' });
-
   } else {
+    Person.find({ name: body.name }).then(person => {
+      if (person) {
+        return response.status(400).json({ error: `${body.name} already exists` });
+      }
+    });
+
     const person = new Person({
       name: body.name,
       number: body.number,
     });
-    person.save().then(savedPerson => {
-      response.json(savedPerson);
-    })
+    person.save()
+      .then(savedPerson => {
+        response.json(savedPerson);
+      })
+      .catch(error => next(error));
   }
 });
 
-app.put('/api/persons/:id', (request, response) => {
+// Update person
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body;
 
   const person = { name: body.name, number: body.number };
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
     .catch(error => next(error));
 });
 
+// Info page
 app.get('/info', (request, response) => {
   Person.countDocuments({}, (err, count) => {
     const date = new Date();
@@ -93,6 +105,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
